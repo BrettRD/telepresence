d50829bca21085198b6e83f3f863c902d90a14d7
BrettRD <brettrd@brettrd.com>
2020-03-26 13:57:39 +1100

    basic turtle via joystick

---
+++ scripts/turtle_joy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+def callback(stick):
+    vel = Twist()
+    vel.angular.z = stick.axes[3]
+    vel.linear.x = stick.axes[4]
+    pub.publish(vel)
+
+def main():
+    rospy.Subscriber("diggy_joy", Joy, callback)
+    rospy.init_node('turtle_joy', anonymous=True)
+    rospy.spin()
+
+
+
+if __name__ == '__main__':
+    pub = rospy.Publisher('diggy_twist', Twist, queue_size=10)
+    main()
