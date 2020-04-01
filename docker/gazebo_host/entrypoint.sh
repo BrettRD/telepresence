2c907e593b6f457f9b94443e44643ce1ee7fb3e7
BrettRD <brettrd@brettrd.com>
2020-04-02 00:55:26 +1100

    added xvfb, doesn't crash, still no video

--- docker/gazebo_host/entrypoint.sh
+++ docker/gazebo_host/entrypoint.sh
@@ -9,4 +9,14 @@ source "/usr/share/gazebo/setup.sh"
 
 #gzserver --verbose &
 
+# Start virtual X server in the background
+# - DISPLAY default is :99, set in dockerfile
+# - Users can override with `-e DISPLAY=` in `docker run` command to avoid 
+#   running Xvfb and attach their screen
+if [[ -x "$(command -v Xvfb)" && "$DISPLAY" == ":99" ]]; then
+	echo "Starting Xvfb" 
+	Xvfb :99 -screen 0 1600x1200x24+32 &
+fi
+
+
 exec "$@"
