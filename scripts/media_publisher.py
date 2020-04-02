e044cbfa281085eef56f868af777516ca7d3def2
BrettRD <brettrd@brettrd.com>
2020-04-03 00:30:38 +1100

    clean up video format conversion

--- scripts/media_publisher.py
+++ scripts/media_publisher.py
@@ -46,24 +46,11 @@ class VideoPublisher:
             except MediaStreamError as e:
                 print("run_track failed with " + e)
                 return
-            #print("frame\n")
-            #img = frame.to_ndarray("rgb24")
-            #print(frame.format.name)
             frame = frame.to_rgb()
-            #print(frame.format.name)
             img = frame.to_ndarray()
-            #print("img\n")
             try:
                 msg = self.__br.cv2_to_imgmsg(img)
-                #msg = self.__br.cv2_to_imgmsg(img,  encoding='rgb8')
                 msg.encoding = "rgb8"
             except CvBridgeError as e:
                 print(e)
-            #print("convert\n")
-
             pub.publish(msg)
-
-            # This format conversion is hard won. Most obvious methods to convert the image format seem to hang the converter.
-            # Image format conversions in pyav or cvbridge both silently fail (hang), no exceptions or return values.
-            # the freeze doesn't seem to affect the rest of the async library, the data channel keeps going.
-            # only passthrough seems to work.
